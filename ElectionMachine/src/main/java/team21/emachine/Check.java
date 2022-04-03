018e0c47a2189d9626f00662871ebd5f4cae52cd
jskodova <89833620+jskodova@users.noreply.github.com>
2022-04-03 15:13:36 +0300

    Added check.java
    checking user login

---
+++ ElectionMachine/src/main/java/team21/emachine/Check.java
@@ -0,0 +1,55 @@
+package team21.emachine;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import team21.emachine.Dao.Dao;
+
+/**
+ * Servlet implementation class Check
+ */
+@WebServlet("/Check")
+public class Check extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Check() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("index.html");
+	}
+    @Override
+   	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   		// TODO Auto-generated method stub
+   		response.setContentType("text/html");	
+   		response.setCharacterEncoding("UTF-8");
+   		
+   		Dao dao = new Dao();
+   		
+		String uname = request.getParameter("username");
+		String password = request.getParameter("password");
+	
+		// Create salt and hashed PW
+		String salt = SecurityUtils.getSalt();
+		String hashed = SecurityUtils.getPasswordHashed(password, salt);
+	
+		dao.addUser(uname, hashed, salt);
+	
+		dao.close();
+		response.sendRedirect("index.html");
+		}
+}
