ed41b59d0a7d6f2c0f7dcaa6ff7c2f390b9f3a5a
rkosova <ron.kosova@gmail.com>
2022-04-04 15:18:33 +0300

    Login Funcionality bones

--- ElectionMachine/src/main/java/team21/emachine/LoginPage.java
+++ ElectionMachine/src/main/java/team21/emachine/LoginPage.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import team21.emachine.Dao.Dao;
+
 
 
 @WebServlet(
@@ -22,10 +24,26 @@ public class LoginPage extends HttpServlet{
 
 	    response.setContentType("text/html");
 	    response.setCharacterEncoding("UTF-8");
+	   
 	    
-	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/login.html");
-	    rd.include(request, response);
-	    
+	  }
+	  
+	  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+	      throws IOException, ServletException {
+		  
+		Dao conn = new Dao();
+		
+		String username = request.getParameter("uname");
+		String password = request.getParameter("Pass");
+		
+		String salt = SecurityUtils.getSalt();
+		String hashed = SecurityUtils.getPasswordHashed(password, salt);
+		
+		String db_return = conn.getUserPasswordHash(username);
+		
+		if (db_return != null) {
+			// login code
+		}
 	  }
 }
 
